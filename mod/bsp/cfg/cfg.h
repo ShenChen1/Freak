10af2b373e3804f5ae4cbc4b81ec3056830fbac1
Peter Shen <peterchenshen@gmail.com>
2020-08-14 15:52:59 +0800

    bsp: prepare build env
    - Add compile options
    - Add some libs

--- mod/bsp/cfg/cfg.h
+++ mod/bsp/cfg/cfg.h
@@ -1,13 +1,14 @@
 #ifndef __CFG_JSONB_C_API_H__
 #define __CFG_JSONB_C_API_H__
 #include "jsonb_c_type.h"
-extern void jsonb_opt_gsf_board_t(jsonb_opt_e opt, cJSON *json, void *element, size_t size);
+extern void jsonb_opt_gsf_bsp_board_t(jsonb_opt_e opt, cJSON *json, void *element, size_t size);
 typedef struct {
     char model[32];
     char sn[32];
-    char type[32];
-    int snscnt;
-    char sensor[8][32];
-    uint64_t caps;
-} gsf_board_t;
+    char chip[32];
+} gsf_bsp_board_t;
+extern void jsonb_opt_gsf_bsp_t(jsonb_opt_e opt, cJSON *json, void *element, size_t size);
+typedef struct {
+    gsf_bsp_board_t board;
+} gsf_bsp_t;
 #endif /* __CFG_JSONB_C_API_H__ */
