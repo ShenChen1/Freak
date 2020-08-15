6d9635ce94283375126b7ab2170b1a42f3e1de10
Peter Shen <peterchenshen@gmail.com>
2020-08-15 12:15:21 +0800

    utils: prepare build env
    Signed-off-by: Peter Shen <peterchenshen@gmail.com>

--- mod/bsp/src/main.c
+++ mod/bsp/src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#include "cJSON.h"
+#include "cfg/cfg.h"
+#include "json-cfg.h"
+
+gsf_bsp_t g_bsp_cfg = {};
 
 int main()
 {
-    printf("%s\n", cJSON_Version());
+    json_cfg_load("tmp", &g_bsp_cfg, sizeof(gsf_bsp_t), jsonb_opt_gsf_bsp_board_t);
+    json_cfg_save("tmp", &g_bsp_cfg, sizeof(gsf_bsp_t), jsonb_opt_gsf_bsp_board_t);
 }
---
+++ mod/utils/json-cfg/src/json-cfg.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+#include "json-cfg.h"
+
+int json_cfg_load(char *filename, void *cfg, size_t size, jsonb_opt_func_t callback)
+{
+    int ret = 0;
+    char *data = NULL;
+    cJSON *json = NULL;
+    FILE *fp = NULL;
+    size_t len = 0;
+
+    fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        return -ENOENT;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    len = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    data = malloc(len + 1);
+    if (data == NULL) {
+        ret = -ENOMEM;
+        goto end1;
+    }
+    data[size] = '\0';
+
+    if (fread(data, 1, len, fp) < len) {
+        ret = -EIO;
+        goto end2;
+    }
+
+    json = cJSON_Parse(data);
+    if (json == NULL) {
+        ret = -ENOMEM;
+        goto end2;
+    }
+
+    callback(JSONB_OPT_J2S, json, cfg, size);
+    cJSON_Delete(json);
+
+end2:
+    free(data);
+end1:
+    fclose(fp);
+
+    return ret;
+}
+
+int json_cfg_save(char *filename, void *cfg, size_t size, jsonb_opt_func_t callback)
+{
+    int ret = 0;
+    char *data = NULL;
+    cJSON *json = NULL;
+    FILE *fp = NULL;
+
+    fp = fopen(filename, "wb");
+    if (fp == NULL) {
+        return -ENOENT;
+    }
+
+    json = cJSON_CreateObject();
+    if (json == NULL) {
+        ret = -ENOMEM;
+        goto end1;
+    }
+
+    callback(JSONB_OPT_S2J, json, cfg, size);
+    data = cJSON_Print(json);
+    if (json == NULL) {
+        ret = -ENOMEM;
+        goto end2;
+    }
+
+    fprintf(fp, "%s", data);
+    cJSON_Delete(json);
+
+end2:
+    free(data);
+end1:
+    fclose(fp);
+
+    return ret;
+}
