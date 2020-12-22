4c67178f1085a99be08ebc6dc7876405a8e98e45
Peter Shen <peterchenshen@gmail.com>
2020-12-22 10:20:52 +0800

    Freak: fix jsonb generation
    PRE_BUILD
        On Visual Studio Generators, run before any other rules are executed
        within the target. On other generators, run just before PRE_LINK
        commands.

--- cmake/utils/jsonb.cmake
+++ cmake/utils/jsonb.cmake
@@ -1,5 +1,6 @@
-macro(jsonb target config destination)
-    add_custom_command(TARGET ${target} PRE_BUILD
+macro(jsonb target item config destination)
+    add_custom_target(cfg-${target}-${item}
         COMMAND python ${FREAK_TOOLS_PATH}/jsonb-c/src/jsonb_c.py --source=${config} --installdir=${destination})
     include_directories(${FREAK_TOOLS_PATH}/jsonb-c/inc/)
+    add_dependencies(${target} cfg-${target}-${item})
 endmacro()
